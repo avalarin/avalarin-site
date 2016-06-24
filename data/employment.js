2de22cafae1040e6e4896851df662cdaea9325c9
avalarin <avalarin@gmail.com>
2016-06-24 21:27:48 +0600

    fix indentation typos

--- data/employment.js
+++ data/employment.js
@@ -1,15 +1,15 @@
 module.exports = function(l10n) {
-    var rows = [];
-    var row = [];
-    l10n.resume.employment.forEach(function(item) {
-        row.push(item);
-        if (row.length == 2) {
-            rows.push(row);
-            row = [];
-        }
-    });
-    if (row.length) {
-        rows.push(row);
+  var rows = [];
+  var row = [];
+  l10n.resume.employment.forEach(function(item) {
+    row.push(item);
+    if (row.length == 2) {
+      rows.push(row);
+      row = [];
     }
-    return rows;
+  });
+  if (row.length) {
+    rows.push(row);
+  }
+  return rows;
 }
